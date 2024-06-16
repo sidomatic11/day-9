53a15bb9909c04220960eaea0b6986d1314d33e5
Siddhant Patil <sidomatic11@gmail.com>
2024-06-16 20:28:09 +0530

    back to dist

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,12 @@ export default {
 		open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
 	},
 	build: {
-		outDir: "../docs", // Output in the dist/ folder
+		outDir: "../dist", // Output in the dist/ folder
 		emptyOutDir: true, // Empty the folder first
 		sourcemap: true, // Add sourcemap
 	},
 	plugins: [
 		restart({ restart: ["../static/**"] }), // Restart server on static file change
 	],
+	base: "/day-9/",
 };
